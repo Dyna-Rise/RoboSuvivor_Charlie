0544af7c6adcfa4d1e2ae66b5c3f0084e9c068ad
kunren <kunren3430@gmail.com>
2025-10-15 15:27:06 +0900

    攻撃コルーチン、接近限界の処理まで

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,5 +1,8 @@
+using System.Collections;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
 public class EnemyController : MonoBehaviour
 {
@@ -31,26 +34,22 @@ public class EnemyController : MonoBehaviour
     //削除される基準のY座標値
     public float deletePosY = -50f;
 
-    public GameObject bulletPrefab; // 発射する弾のPrefab
+    public GameObject bulletPrefab;    // 発射する弾のPrefab
     public GameObject gate;            // 弾を発射する位置
-    //public Transform firePoint;          // 弾が発射される位置と向き
-    public float bulletSpeed = 100f;    // 発射する弾の速度 
-    public float fireInterval = 2.0f; //弾を発射するインターバル
-    private float nextFireTime;          // 次に発射可能な時間
-    //public float rotationSpeed = 5f;     // プレイヤーの方向へ回転する速度 (スムーズなLookAt用)
+    public float bulletSpeed = 100f;   // 発射する弾の速度 
+    public float fireInterval = 2.0f;  // 弾を発射するインターバル
+    //public float rotationSpeed = 5   // プレイヤーの方向へ回転する速度 (スムーズなLookAt用)
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player");
 
-        isDamage = false;
         isAttack = false;
         lockOn = false;
 
         timer = 0f; //経過時間を計測
 
-  
     }
 
 
@@ -59,8 +58,8 @@ public class EnemyController : MonoBehaviour
         //playingモードでないと何もしない
         if (GameManager.gameState != GameState.playing) return;
 
-        //プレイヤーがいない時は何もしない
-        if (player == null) return;
+        //プレイヤーがいない時 or エネミーのHPが0の時は何もしない
+        if (player == null || enemyHP <= 0) return;
 
         //ステージ外に落ちたら消滅
         if (transform.position.y <= deletePosY)
@@ -69,35 +68,75 @@ public class EnemyController : MonoBehaviour
             return;
         }
 
-        float distance = Vector3.Distance(transform.position, player.transform.position);
-        if (distance < detectionRange) //索敵範囲に入ったら変数enemySpeedのスピードで近づいてくる
+        if (lockOn) //ターゲットを向く
         {
+            transform.LookAt(player.transform.position); // これだと瞬時に回転
+            //LookAtPlayer(); // スムーズな回転
+        }
+
+        if(isAttack) //攻撃中なら何もしない
+        {
+            return;
+        }
+
+        //プレイヤーとの距離を常に測る
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distance <= detectionRange) //索敵範囲に入ったら変数enemySpeedのスピードで近づいてくる
+        {
+            lockOn = true;
             navMeshAgent.isStopped = false;
             navMeshAgent.SetDestination(player.transform.position);
             navMeshAgent.speed = enemySpeed;
 
             //射程範囲に入ったら攻撃しつつゆっくり近づいいてくる
-            if (distance < attackRange)
+            if (distance <= attackRange)
             {
-                transform.LookAt(player.transform.position); // これだと瞬時に回転
-                //LookAtPlayer(); // スムーズな回転
+                
                 navMeshAgent.speed = enemySpeed * 0.5f; //半分の速度にする
 
                 if (Time.time >= timer)
                 {
-                    Shoot(); // 弾を発射
-                    timer = Time.time + 1f / fireInterval; // 次の発射可能時間を更新
+                    StartCoroutine(Attack());
+
+                    //timer = Time.time + 1f / fireInterval; // 次の発射可能時間を更新
                 }
+            }
+            if(distance <= stopRange)
+            {
+                navMeshAgent.isStopped = true;
             }
         }
         else
         {
             navMeshAgent.isStopped = true;
         }
+
+
+
+        if (isDamage)
+        {
+            StartCoroutine(Damaged());
+        }
+        
+
     }
 
+    
+    IEnumerator Attack()
+    {
+        isAttack = true;
+        lockOn = false; // 攻撃中もプレイヤーの方を向かせたいならコメントアウト
+        Shot();
+        yield return new WaitForSeconds(fireInterval);
+        isAttack = false;
+        lockOn = true;
+        timer = 0f;  
+    }
+
+
     // 弾を発射するメソッド
-    void Shoot()
+    void Shot()
     {
         //enemyが消滅していれば何もしない
         if (gameObject == null) return;
@@ -122,22 +161,22 @@ public class EnemyController : MonoBehaviour
             //生成した球のAddForceの力でshoot
             rbody.AddForce(v, ForceMode.Impulse);
         }
-
     }
 
 
     //接触判定
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        //ぶつかった相手がBulletなら
-        if (hit.gameObject.CompareTag("Bullet"))
+        //ぶつかった相手がPlayerBulletなら
+        if (hit.gameObject.CompareTag("PlayerBullet"))
         {
+            isDamage = true;
+
             //体力をマイナス
             enemyHP--;
 
             //ダメージ音を鳴らす
             //SEPlay(SEType.Damage);
-
 
             if (enemyHP <= 0)
             {
@@ -145,17 +184,56 @@ public class EnemyController : MonoBehaviour
                 Destroy(gameObject, 0.5f); //少し時間差で自分を消滅
             }
 
-            //接触したBulletを削除
+            //接触したPlayerBulletを削除
             Destroy(hit.gameObject);
+        }
+
+        //ぶつかった相手がPlsyerSwordなら
+        if (hit.gameObject.CompareTag("PlayerSword"))
+        {
+            isDamage = true;
+
+            //体力を3マイナス
+            enemyHP -= 3;
+
+            //ダメージ音を鳴らす
+            //SEPlay(SEType.Damage);
+
+            if (enemyHP <= 0)
+            {
+                //Instantiate(bom, transform.position, Quaternion.identity); //爆発エフェクトの発生
+                Destroy(gameObject, 0.5f); //少し時間差で自分を消滅
+            }
+
         }
     }
 
+
+    IEnumerator Damaged()
+    {
+        Blinking();
+        yield return new WaitForSeconds(2);
+
+        isDamage = false;
+    }
+
+    //点滅処理
+    void Blinking()
+    {
+        //その時のゲーム進行時間で正か負かの値を算出
+        float val = Mathf.Sin(Time.time * 50);
+        //正の周期なら表示
+        if (val >= 0) body.SetActive(true);
+        //負の周期なら非表示
+        else body.SetActive(false);
+    }
 
     // ギズモで範囲を表示（デバッグ用）
     void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere(transform.position, detectionRange);
         Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.DrawWireSphere(transform.position, stopRange);
     }
 
 
